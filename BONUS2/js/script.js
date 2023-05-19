0b256eb5c02213ad15c30feb6e7a042a52abb4b7
quintusdev <quintusdev92@gmail.com>
2023-05-19 17:04:41 +0200

    [STYLE] Aggiunto style alla pagina html

--- BONUS2/js/script.js
+++ BONUS2/js/script.js
@@ -1,15 +1,21 @@
 //DICHIARAZIONE VARIABILI
 "use strict";
+
 //ASSEGNO ELEMENTO UL DAL DOM
 const lista = document.querySelector('ul');
+
 //CICLO FOR
 for (let num = 1; num<= 100; num++) {
+
     //CREO UN NUOVO LI OGNI VOLTA CHE ENTRO NEL CICLO
     const li = document.createElement('li');
+
     //ASSEGNO AD LI LA VARIABILE NUM
     li.append(num);
+
     //ASSEGNO A LISTA LA VARIABILE LI
     lista.append(li);
+    
     //EFETTUO IL CONTROLLO
     if (num % 15 === 0) {
         li.innerHTML = 'FizzBuzz';
@@ -20,8 +26,8 @@ for (let num = 1; num<= 100; num++) {
     }else if (num % 5 === 0) {
         li.innerHTML = 'Buzz';
     }
-
 }
+
 
     
 
